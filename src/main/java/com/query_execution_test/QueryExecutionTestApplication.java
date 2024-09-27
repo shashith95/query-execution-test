7132c1fafe762f3e7de3b63976c985d258e5f0b3
Shashith <Shashith.Aravinda@cloudsolutions.com.sa>
2024-09-27 10:49:52 +0530

    initial commit

---
+++ src/main/java/com/query_execution_test/QueryExecutionTestApplication.java
@@ -0,0 +1,13 @@
+package com.query_execution_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryExecutionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryExecutionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/query_execution_test/QueryExecutionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.query_execution_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryExecutionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
